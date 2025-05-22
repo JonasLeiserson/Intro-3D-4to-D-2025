904d8e0fc1d8676d2d6cb437391db9f299e4e5b0
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-05-22 10:43:24 -0300

    hoka

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "player")
+        Destroy(gameObject);
+    }
+}
