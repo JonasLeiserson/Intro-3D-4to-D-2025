e2f52572cf3a90373b25ca1e12ae1cf47cd8496d
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-06-09 10:39:22 -0300

    lpl

---
+++ Assets/EsperarSegundos.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EsperarSegundos : MonoBehaviour
+{
+    public float tiempoDeEspera;
+    float timeElapsed;
+    public Transform cuboTR;
+    void Start()
+    {
+        //InvokeRepeating(nameof(ElevarCubo), 0, tiempoDeEspera);
+        StartCoroutine(EsperarAlaElevacion());
+    }
+    void Update()
+    {
+        //Debug.Log(Time.deltaTime);
+        
+        //if (timeElapsed >= tiempoDeEspera)
+        //{
+        //    Debug.Log("Tiempo cumplido");
+        //    timeElapsed = 0;
+        //    ElevarCubo();
+        //}
+        //else
+        //{
+        //    Debug.Log(timeElapsed += Time.deltaTime);
+        //}
+    }
+    IEnumerator EsperarAlaElevacion()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(tiempoDeEspera);
+            ElevarCubo();
+        }
+    }
+    void ElevarCubo()
+    {
+        cuboTR.position += Vector3.up * 1;
+    }
+}
---
+++ Assets/Scripts/TiempoManger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiempoManger : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+    void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate");
+    }
+    void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+}
